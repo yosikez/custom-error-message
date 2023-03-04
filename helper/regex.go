dfa73e17e3a32bbfda5e4e7aa305292c185c1eda
yosikez <yosikez@gmail.com>
2023-03-04 11:20:48 +0700

    first commit

---
+++ helper/regex.go
@@ -0,0 +1,36 @@
+package helper
+
+import (
+	"reflect"
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
+
+
+func CheckDiveField(err validator.FieldError, parent interface{}, child interface{}) (map[string]string, bool) {
+	pattern := `^(\w+)\.(\w+)\[(\d+)\]\.(\w+)$`
+	re := regexp.MustCompile(pattern)
+	str := err.Namespace()
+
+	isTrue := re.MatchString(str)
+
+	if !isTrue {
+		return nil, false
+	}
+
+	result := re.FindStringSubmatch(str)
+	mapStr := make(map[string]string)
+
+	fieldJson, _ :=  reflect.TypeOf(parent).FieldByName(result[2])
+	field := GetJSONTagName(fieldJson)
+
+	attributJson, _ := reflect.TypeOf(child).FieldByName(result[4])
+	attrField := GetJSONTagName(attributJson)
+	
+	mapStr["field"] = field
+	mapStr["index"] = result[3]
+	mapStr["attribute"] = attrField
+
+	return mapStr, true
+}
